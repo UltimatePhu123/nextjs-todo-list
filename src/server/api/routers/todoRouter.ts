c7402a206f47a798317887594de0e9b3d8eaadac
Phat Phu <phatphunguyen18@gmail.com>
2023-07-13 23:54:37 +0700

    Fixed the trpc

--- src/server/api/routers/todoRouter.ts
+++ src/server/api/routers/todoRouter.ts
@@ -1,37 +1,39 @@
-import { z } from "zod"
-import { 
-  createTRPCRouter, 
-  protectedProcedure 
-} from "~/server/api/trpc"
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const todoRouter = createTRPCRouter({
   getAll: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.todo.findMany({
       where: {
-        userId: ctx.session.user.id
-      }
-    })
+        userId: ctx.session.user.id,
+      },
+    });
   }),
 
   create: protectedProcedure
-    .input(z.object({ title: z.string(), isChecked: z.boolean() }))
+    .input(
+      z.object({
+        title: z.string(),
+        isChecked: z.boolean() || z.null() || z.undefined(),
+      })
+    )
     .mutation(({ ctx, input }) => {
       return ctx.prisma.todo.create({
         data: {
           title: input.title,
-          isChecked: input.isChecked,
-          userId: ctx.session.user.id
-        }
-      })
+          isChecked: input?.isChecked,
+          userId: ctx.session.user.id,
+        },
+      });
     }),
 
-    delete: protectedProcedure
+  delete: protectedProcedure
     .input(z.object({ id: z.string() }))
     .mutation(({ ctx, input }) => {
       return ctx.prisma.todo.delete({
         where: {
-          id: input.id
-        }
-      })
-    })
-})
+          id: input.id,
+        },
+      });
+    }),
+});
